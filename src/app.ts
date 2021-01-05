b43748b5f7b367fbe7d36d3be56bd0b46ddc0d5a
Myo Maung <myomaung5255225@gmail.com>
2021-01-06 06:25:12 +0630

    cors fixed

--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,16 @@ const options: cors.CorsOptions = {
   preflightContinue: false,
 };
 
+app.use(function (_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+});
+
 //use cors middleware
 app.use(cors(options));
 
